e01ee79c79a4d580c990f3bd36d6b70e85e14ebe
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-08-24 20:38:25 +0530

    add getter and setter methods to all fields in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,58 @@ public class Book {
     private String bookName;
     private String bookAuthor;
     private String bookISBNNumber;
+
+    /**
+     * This method returns the name of the book.
+     *
+     * @return The name of the book.
+     */
+    public String getBookName() {
+        return bookName;
+    }
+
+    /**
+     * This method sets the name of the book.
+     *
+     * @param bookName The name of the book you want to set.
+     */
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    /**
+     * This method return the name of the author of the book.
+     *
+     * @return The name of the author of the book.
+     */
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    /**
+     * This method sets the name of the author of the book.
+     *
+     * @param bookAuthor The name of the author of the book you want to set.
+     */
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    /**
+     * This method return the 13-digit ISBN number of the book.
+     *
+     * @return The 13-digit ISBN number of the book.
+     */
+    public String getBookISBNNumber() {
+        return bookISBNNumber;
+    }
+
+    /**
+     * This method sets the 13-digit ISBN number of the book.
+     *
+     * @param bookISBNNumber The 13-digit ISBN number of the book you want to set.
+     */
+    public void setBookISBNNumber(String bookISBNNumber) {
+        this.bookISBNNumber = bookISBNNumber;
+    }
 }

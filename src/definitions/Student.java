52635f9be589149792fd607cfc6deaf0589633de
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-08-25 13:14:53 +0530

    add getter and setter methods to all the fields in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,8 +7,80 @@
 package definitions;
 
 public class Student {
-    private String studentName;
+    private String studentName; // (first name, middle name, last name)
     private long studentUniversityRollNumber;
     private int numberOfBooksIssuedByStudent;
     private String[] studentIssuedBooksName;
+
+    /**
+     * This method returns the name of the student.
+     *
+     * @return The name of the student.
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * This methods sets the name of the student.
+     *
+     * @param studentName The name of the student you want to set.
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * This method return the university roll number of the student.
+     *
+     * @return The university roll number of the student.
+     */
+    public long getStudentUniversityRollNumber() {
+        return studentUniversityRollNumber;
+    }
+
+    /**
+     * This method sets the university roll number of the student.
+     *
+     * @param studentUniversityRollNumber The university roll number of the student you want to set.
+     */
+    public void setStudentUniversityRollNumber(long studentUniversityRollNumber) {
+        this.studentUniversityRollNumber = studentUniversityRollNumber;
+    }
+
+    /**
+     * This method returns the number of the books issued by the student.
+     *
+     * @return The number of the books issued by the student.
+     */
+    public int getNumberOfBooksIssuedByStudent() {
+        return numberOfBooksIssuedByStudent;
+    }
+
+    /**
+     * This method sets the number of the books issued by the student.
+     *
+     * @param numberOfBooksIssuedByStudent The number of the books issued by the student.
+     */
+    public void setNumberOfBooksIssuedByStudent(int numberOfBooksIssuedByStudent) {
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+    }
+
+    /**
+     * This method returns the names of all the books issued by the student.
+     *
+     * @return The names of all the books issued by the student.
+     */
+    public String[] getStudentIssuedBooksName() {
+        return studentIssuedBooksName.clone();
+    }
+
+    /**
+     * This method sets the names of all the books that are issued by the student.
+     *
+     * @param studentIssuedBooksName The names of all the books that are issued by student.
+     */
+    public void setStudentIssuedBooksName(String[] studentIssuedBooksName) {
+        this.studentIssuedBooksName = studentIssuedBooksName;
+    }
 }

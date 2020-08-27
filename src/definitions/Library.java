b73656fe7300c01c9f7b59845ac86c59751c5754
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-08-27 14:30:59 +0530

    add getter and setter methods to all fields in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,22 @@ package definitions;
 
 public class Library {
     private Book[] booksCurrentlyAvailable;
+
+    /**
+     * This method returns the names of all the books which are currently available in the Library.
+     *
+     * @return The names of all books which are currently available.
+     */
+    public Book[] getBooksCurrentlyAvailable() {
+        return booksCurrentlyAvailable.clone();
+    }
+
+    /**
+     * This method sets the names of all the books which are currently available in the library.
+     *
+     * @param booksCurrentlyAvailable The names of all the books which are currently available.
+     */
+    public void setBooksCurrentlyAvailable(Book[] booksCurrentlyAvailable) {
+        this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+    }
 }
